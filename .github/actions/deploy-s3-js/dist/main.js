6bd9fb68a9e60d8f5873324ed5c20c14002c2b96
yardiewizardie <yardiewizardie@gmail.com>
2023-07-16 13:01:01 -0400

    change build

--- .github/actions/deploy-s3-js/dist/main.js
+++ .github/actions/deploy-s3-js/dist/main.js
@@ -1,5 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@actions/core");
 const run = () => {
+    (0, core_1.notice)("Hello from the future");
 };
 run();
