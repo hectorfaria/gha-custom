8ed184610d780b5081ad8ebbcd952572e904b48a
yardiewizardie <yardiewizardie@gmail.com>
2023-07-16 14:52:50 -0400

    added output

--- .github/actions/deploy-s3-js/main.ts
+++ .github/actions/deploy-s3-js/main.ts
@@ -1,11 +1,12 @@
-import { notice, getInput } from "@actions/core";
+import { setOutput, getInput } from "@actions/core";
 import { exec } from "@actions/exec";
 const run = () => {
     const bucket = getInput('bucket', { required: true })
     const bucketRegion = getInput('bucket-region', { required: true })
     const distFolder = getInput('dist-folder', { required: true })
     exec(`aws s3 sync ${distFolder} s3://${bucket} --region ${bucketRegion}`)
-    notice("Hello from the future")
+    
+    setOutput('website-url', `${bucket}.s3-website-${bucketRegion}.amazonaws.com`)
 }
 
 run();
